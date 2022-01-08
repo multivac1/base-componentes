a682fe281f5516fe7c9f8b57ad16aaedd954bc95
nfoster <nfoster@glamit.com.ar>
2022-01-08 02:38:22 -0300

    finalizando desarrollo componente para tabla de talles

--- componentes/react/components/productSize/index.tsx
+++ componentes/react/components/productSize/index.tsx
@@ -10,43 +10,59 @@ import styles from './styles.css';
 interface TriggerI {
     triggerText: string,
     triggerColor: string,
-    triggerWeight: string
+    triggerWeight: number,
+    attributeName: string
 }
 
 interface OptionsI {
     TRIGGER_TITLE: string,
     TRIGGER_COLOR: string,
-    TRIGGER_WEIGHT: string
+    TRIGGER_WEIGHT: number,
+    ATTRIBUTE_NAME: string 
 }
 
 const OPTIONS: OptionsI = {
     TRIGGER_TITLE: "Tabla de Talles",
     TRIGGER_COLOR: "#000",
-    TRIGGER_WEIGHT: "400"
+    TRIGGER_WEIGHT: 400,
+    ATTRIBUTE_NAME: ""
 }
 
 const ProductSize = (props: TriggerI) => {
-    const { triggerText, triggerColor, triggerWeight } = props;
+    let { triggerText, triggerColor, triggerWeight, attributeName } = props;
     const {product} = useContext(ProductContext);
     // almacena atributo de producto para tabla de talle
-    // producto c/atributo para test: https://practicacomponentes--herenciaar.myvtex.com/3837561171hb-remera-hot-ride-307/p
+    // producto c/atributo para test prendas superiores: https://practicacomponentes--herenciaar.myvtex.com/3837561171hb-remera-hot-ride-307/p
+    // producto test boys: https://practicacomponentes--herenciaar.myvtex.com/3861161171cr-remera-rat-rod-317/p
     // prod s/atributo: https://practicacomponentes--herenciaar.myvtex.com/3866621101az-gorra-herencia-108/p?skuId=41258
-    const tableName = product?.properties[2]?.values[0];
-    console.log('Datos producto:');    
+    let properties = product?.properties;
+    let filteredAtribute = properties?.filter(property => property?.name == "Tabla de talles").map(item => (
+        item?.values[0]
+    ));
+    let tableName = filteredAtribute?.toString().toLowerCase().replace(/ /g, ""); 
+    const arrayName = attributeName?.split(',');
+
+    console.log('tableName');
     console.log(tableName);
+
+    console.log('arrayName');
+    console.log(arrayName);
+
+    console.log('resultado');
+    console.log(arrayName.includes(tableName));
     
     return useMemo(() => {
         return (
             <>
                 {
-                    tableName ?
+                   arrayName.includes(tableName)  ?
                         <EnhancedModalTrigger>
                             <div className={styles.TriggerTitle} style={{color: triggerColor, fontWeight: triggerWeight}}>
                                 <MemoizedRichText text={triggerText} />
                             </div>
                             <ModalRef fullScreen="true">
                                 <ModalHeader iconCloseSize="36" />
-                                <Image src="https://herenciaar.vteximg.com.br/arquivos/prendas_superiores.jpg" />
+                                <Image src={`https://herenciaar.vteximg.com.br/arquivos/${tableName}.jpg`} />
                             </ModalRef>
                         </EnhancedModalTrigger>  
                     : <Fragment />
@@ -75,8 +91,14 @@ ProductSize.schema = {
         triggerWeight: {
             title: "Ingresar algún valor de las descripción",
             description: "300, 400, 500, 600, 700",
-            type: "string",
+            type: "number",
             default: OPTIONS.TRIGGER_WEIGHT
+        },
+        attributeName: {
+            title: "Nombre del atributo",
+            description: "sin espacios y en minúsculas. Ej: prendassuperiores",
+            type: "string",
+            default: OPTIONS.ATTRIBUTE_NAME
         }
     }
 }
@@ -84,7 +106,8 @@ ProductSize.schema = {
 ProductSize.defaultProps = {
     triggerText: OPTIONS.TRIGGER_TITLE,
     triggerColor: OPTIONS.TRIGGER_COLOR,
-    triggerWeight: OPTIONS.TRIGGER_WEIGHT
+    triggerWeight: OPTIONS.TRIGGER_WEIGHT,
+    attributeName: OPTIONS.ATTRIBUTE_NAME
 }
 
 export default ProductSize;
