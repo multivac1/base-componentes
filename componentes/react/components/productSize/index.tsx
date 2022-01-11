86edec7af680fd18c361141d5c06e04a98167fde
nfoster <nfoster@glamit.com.ar>
2022-01-11 17:07:40 -0300

    nuevas mejoras componente

--- componentes/react/components/productSize/index.tsx
+++ componentes/react/components/productSize/index.tsx
@@ -5,62 +5,58 @@ import ModalRef from 'vtex.modal-layout/Modal';
 import ModalHeader from 'vtex.modal-layout/ModalHeader';
 import MemoizedRichText from 'vtex.rich-text/index';
 import Image from 'vtex.store-image/Image';
-import styles from './styles.css';
+import { useRuntime } from 'vtex.render-runtime';
+//import styles from './styles.css';
 
 interface TriggerI {
     triggerText: string,
     triggerColor: string,
-    triggerWeight: number,
-    attributeName: string
+    triggerWeight: number
 }
 
 interface OptionsI {
     TRIGGER_TITLE: string,
     TRIGGER_COLOR: string,
-    TRIGGER_WEIGHT: number,
-    ATTRIBUTE_NAME: string 
+    TRIGGER_WEIGHT: number
 }
 
 const OPTIONS: OptionsI = {
     TRIGGER_TITLE: "Tabla de Talles",
     TRIGGER_COLOR: "#000",
-    TRIGGER_WEIGHT: 400,
-    ATTRIBUTE_NAME: ""
+    TRIGGER_WEIGHT: 400
 }
 
 const ProductSize = (props: TriggerI) => {
-    let {product} = useContext(ProductContext);
-    let { triggerText, triggerColor, triggerWeight, attributeName } = props;
+    //let {product} = useContext(ProductContext);
+    //let { triggerText, triggerColor, triggerWeight } = props;
+    const runtime = useRuntime();
     // producto c/atributo para test prendas superiores: https://practicacomponentes--herenciaar.myvtex.com/3837561171hb-remera-hot-ride-307/p
     // producto test boys: https://practicacomponentes--herenciaar.myvtex.com/3861161171cr-remera-rat-rod-317/p
     // prod s/atributo: https://practicacomponentes--herenciaar.myvtex.com/3866621101az-gorra-herencia-108/p?skuId=41258
-    let properties = product?.properties;
-    let filteredAtribute = properties?.filter(property => property?.name == "Tabla de talles").map(item => (item?.values[0]));
-    let tableName = filteredAtribute?.toString().toLowerCase().replace(/ /g, ""); 
-    let arrayNames = attributeName?.split(',');
+    //let properties = product?.properties;
+    //let filteredAtribute = properties?.filter((property:any) => property?.name == "Tabla de talles").map((item:any) => (item?.values[0]));
+    //let tableName = filteredAtribute?.toString().toLowerCase().replace(/ /g, ""); 
+   // let arrayNames = attributeName?.split(',');
 
     console.log('Nombre tablita');
-    console.log(tableName);
+    console.log(props);
 
-    console.log('Array');
-    console.log(arrayNames);
-    
     return useMemo(() => {
         return (
             <>
-                {
-                   arrayNames.includes(tableName)  ?
-                        <EnhancedModalTrigger>
+                
+                   
+                        {/* <EnhancedModalTrigger>
                             <div className={styles.TriggerTitle} style={{color: triggerColor, fontWeight: triggerWeight}}>
                                 <MemoizedRichText text={triggerText} />
                             </div>
                             <ModalRef fullScreen="true">
                                 <ModalHeader iconCloseSize="36" />
-                                <Image src={`https://herenciaar.vteximg.com.br/arquivos/${tableName}.jpg`} />
+                                <Image src={`https://${runtime?.account}.vteximg.com.br/arquivos/${tableName}.jpg`} />
                             </ModalRef>
-                        </EnhancedModalTrigger>  
-                    : <Fragment />
-                }
+                        </EnhancedModalTrigger>   */}
+                    
+                
             </>
         )
     }, [props])
@@ -80,19 +76,41 @@ ProductSize.schema = {
             title: "Color del texto",
             description: "# Exadecimal",
             type: "string",
+            widget: {
+                "ui:widget": "color"
+            },        
             default: OPTIONS.TRIGGER_COLOR
         },
         triggerWeight: {
             title: "Ingresar algún valor de las descripción",
             description: "300, 400, 500, 600, 700",
-            type: "number",
+            type: "string",
+            enum: ["400", "700"],
+            enumNames: ["normal", "negrita"],
             default: OPTIONS.TRIGGER_WEIGHT
         },
-        attributeName: {
-            title: "Nombre del atributo",
-            description: "sin espacios y en minúsculas. Ej: prendassuperiores",
-            type: "string",
-            default: OPTIONS.ATTRIBUTE_NAME
+        campoArray: {
+            title: "Tablas",
+            type: "array", 
+            items: {
+                title: "Tablas",
+                type: "object",
+                properties: {
+                    nombreTabla: {
+                        title: "Nombre de la tabla",
+                        description: "Poné el nombre",
+                        type: "string"
+                    },
+                    imagenTabla: {
+                        title: "Url imagen",
+                        description: "Poné la imagen",
+                        type: "string",
+                        widget: {
+                            "ui:widget" : "image-uploader"
+                        }                
+                    }
+                }
+            }
         }
     }
 }
@@ -100,8 +118,7 @@ ProductSize.schema = {
 ProductSize.defaultProps = {
     triggerText: OPTIONS.TRIGGER_TITLE,
     triggerColor: OPTIONS.TRIGGER_COLOR,
-    triggerWeight: OPTIONS.TRIGGER_WEIGHT,
-    attributeName: OPTIONS.ATTRIBUTE_NAME
+    triggerWeight: OPTIONS.TRIGGER_WEIGHT
 }
 
 export default ProductSize;
